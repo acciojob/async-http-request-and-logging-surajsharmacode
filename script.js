6e4abd194b07151161fd048e90c38916ac29b24f
suraj sharma <94784024+surajsharmacode@users.noreply.github.com>
2024-01-12 00:18:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let API = "https://jsonplaceholder.typicode.com/todos/1"
+async function fetchData(){
+	const res = await fetch(API);
+	const data = res.json();
+	console.log(data);
+}
+fetchData();
